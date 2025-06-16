e107822922cc2e9d250a733832bf90c413367d8a
SarahLi25 <sarahliwork25@gmail.com>
2025-06-16 13:44:41 -0500

    practice

--- arrays.js
+++ arrays.js
@@ -1,39 +1,39 @@
 // CODE ALONG
 
 // 1. Update the variable songList so that it is an array with at least 3 song titles.
-let songList;
+let songList= ["Shape of You", "Perfect", "Happier", "The Nights"]
 console.log();
 
 // 2. Update the variable studentList so that it is an array with 4 Code Nation students (including yourself) in your class.
-let studentList;
-console.log();
+let studentList= ["Alice", "Bob", "Charlie", "Sarah"]
+console.log(studentList);
 
 // 3. Update the variable teacherList so that it is an array with all the Code Nation adults in your class.
-let teacherList;
-console.log();
+let teacherList= ["Mr. Smith", "Ms. Johnson", "Mrs. Brown"]
+console.log(teacherList);
 
 // 4. Update the variable familyAges so that it is an array with a list of ages (numbers) of your family members.
-let familyAges;
-console.log();
+let familyAges= [45, 42, 18, 16, 12]
+console.log(familyAges);
 
 //PAUSE HERE
 
 // CODE SOLO
 
 // 5. Update the variable favSong to retrieve the array element of your favorite song in the songList array (Task 1). 
-let favSong;
+let favSong= studentList[3]
 console.log();
 
 // 6. Update the variable bestStudent to retrieve your name from the classList array (Task 2).
-let bestStudent;
+let bestStudent= studentList[2]
 console.log();
 
 // 7. Update the variable tallestTeacher to retrieve the array element of the tallest teacher in the teacherList array (Task 3).
-let tallestTeacher;
+let tallestTeacher= teacherList[3]
 console.log();
 
 // 8. Update the variable oldestMemeber to retrieve the array element of the oldest member of your family in the familyAges array. (Task 4)
-let oldestMember;
+let oldestMember= familyAges[1]
 console.log();
 
 // Done Early?
